3cf09eb141ba5a7112487afc9e0e328cd1d4991e
gabester0 <gceipper@gmail.com>
2022-09-25 14:51:56 -0400

    removig dotenv - not needed already handled by CRA

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
-import dotenv from 'dotenv';
 import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,7 +9,6 @@ import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-dotenv.config();
 
 root.render(
   <React.StrictMode>
