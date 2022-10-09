da33241e64a0313af8a0f585a91f7c6a46e0ae51
gabester0 <gceipper@gmail.com>
2022-10-09 17:28:25 -0400

    updating app title and styling error page

--- src/Error/Error.tsx
+++ src/Error/Error.tsx
@@ -1,16 +1,25 @@
 import { useRouteError } from "react-router-dom";
+import { ErrorContainer } from "./styled";
+
+interface CustomError extends Error {
+  statusText: string;
+  status: number;
+}
 
 export default function Error() {
-  const error = useRouteError() as Error;
+  const error = useRouteError() as CustomError;
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.message || JSON.stringify(error)}</i>
-      </p>
-    </div>
+    <ErrorContainer id="error-page">
+      <div>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <p>{error.status && <i> Status code: {error.status}</i>}</p>
+      </div>
+    </ErrorContainer>
   );
 }
